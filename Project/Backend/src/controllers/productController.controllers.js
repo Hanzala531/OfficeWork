495523b31082f007f9bf876234afd17f0129c7dc
Hanzala531 <165744107+Hanzala531@users.noreply.github.com>
2025-01-26 14:12:57 +0500

    worked  on controllers

--- Project/Backend/src/controllers/productController.controllers.js
+++ Project/Backend/src/controllers/productController.controllers.js
@@ -1,3 +1,19 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
-import Product from '../models/product.models.js'
+import {Product} from '../models/product.models.js'
+import {Category} from '../models/category.models.js'
 
+// Fetching all the products 
+const getAllProducts = asyncHandler(async (req , res )=> {
+    try {
+    console.log("Fetching all the products");
+    const products = await Product.find().populate('category');
+    console.log(`Found ${products.length} products `);
+    res.json(products);
+    }
+    catch (error) {
+        console.log("Error in finding products",error);
+        res.status(500).json({message: "Internal Server Error" })
+    }
+});
+
+// Creating a new product

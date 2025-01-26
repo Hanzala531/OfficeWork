c022ddb3d44db89d4c43fb714cbe5035ee56aa1d
Hanzala531 <165744107+Hanzala531@users.noreply.github.com>
2025-01-27 00:04:01 +0500

    modified:   Project/Backend/src/aap.js 	modified:   Project/Backend/src/index.js: 	modified:   Project/Backend/src/db/index.js

--- Project/Backend/src/aap.js
+++ Project/Backend/src/aap.js
@@ -1,3 +1,35 @@
 import express , {urlencoded} from 'express';
 import cors from 'cors';
-import cookieparser from 'cookie-parser'
+import cookieparser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors(
+    {
+        origin : process.env.CORS_ORIGIN,
+        credentials : true
+    }
+))
+
+app.use(express.json(
+    {
+        limit : "30kb"
+    }
+))
+
+app.use(urlencoded(
+    {
+        extended:true,
+        limit:"16kb"
+    }
+))
+
+app.use(express.static("public"));
+
+app.use(cookieparser());
+
+import router from "./routes/product.route.js"
+
+app.use("api/v1/products" , router);
+
+export {app}
---
+++ Project/Backend/src/index.js
@@ -0,0 +1,21 @@
+import connectDB from './db/index.js';
+import dotenv from 'dotenv';
+import { app } from './app.js'; // Add this line to import the app
+
+dotenv.config({
+    path: './.env'
+});
+
+// Connect to MongoDB
+connectDB()
+
+.then(() => {
+    app.listen(process.env.PORT  || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.log('Error connecting to MongoDB:', error);
+})
+
+

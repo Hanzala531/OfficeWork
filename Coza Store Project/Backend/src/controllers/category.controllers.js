620b22a8367292e9e838f4057ecac66ddd41f424
Hanzala531 <165744107+Hanzala531@users.noreply.github.com>
2025-02-11 20:58:41 +0500

    chore: remove unused Cloudinary upload import from category controller

--- Coza Store Project/Backend/src/controllers/category.controllers.js
+++ Coza Store Project/Backend/src/controllers/category.controllers.js
@@ -2,7 +2,6 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { Category } from "../models/category.models.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 // Fetching all the categories
 const getCategories = asyncHandler(async (req, res) => {
